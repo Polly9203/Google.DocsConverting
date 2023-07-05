841f55a938aaf9c9f9010282399a7b332460685e
Polly <apollinaria.9203@yandex.by>
2023-07-06 01:47:38 +0300

    Added configurations to appsettings

--- Google.BLL/Convert/Commands/ConvertCommandHandler.cs
+++ Google.BLL/Convert/Commands/ConvertCommandHandler.cs
@@ -3,17 +3,26 @@ using Google.Apis.Drive.v3;
 using Google.Apis.Services;
 using Google.BLL.Convert.Models;
 using MediatR;
+using Microsoft.Extensions.Options;
+using Utils.Constants.Configuration;
 
 namespace Google.BLL.Convert.Commands
 {
     public class ConvertCommandHandler : IRequestHandler<ConvertCommand, ConvertResult>
     {
-        private readonly string serviceAccountKeyPath = @"C:\Users\polin\Just_all\Work\Ilya\DocsConverting\SecurityKey.json";
+        private readonly GoogleConfiguration googleConfiguration;
+        private readonly FileConfiguration fileConfiguration;
+
+        public ConvertCommandHandler(IOptions<GoogleConfiguration> googleConfiguration, IOptions<FileConfiguration> fileConfiguration)
+        {
+            this.googleConfiguration = googleConfiguration.Value;
+            this.fileConfiguration = fileConfiguration.Value;
+        }
 
         public async Task<ConvertResult> Handle(ConvertCommand command, CancellationToken cancellationToken)
         {
             GoogleCredential credential;
-            using (var stream = new FileStream(serviceAccountKeyPath, FileMode.Open, FileAccess.Read))
+            using (var stream = new FileStream(googleConfiguration.ServiceAccountKeyPath, FileMode.Open, FileAccess.Read))
             {
                 credential = GoogleCredential.FromStream(stream)
                     .CreateScoped(DriveService.ScopeConstants.Drive);
@@ -22,19 +31,19 @@ namespace Google.BLL.Convert.Commands
             var service = new DriveService(new BaseClientService.Initializer()
             {
                 HttpClientInitializer = credential,
-                ApplicationName = "Converter"
+                ApplicationName = googleConfiguration.ApplicationName
             });
 
             var fileMetadata = new Apis.Drive.v3.Data.File()
             {
                 Name = Path.GetFileName(command.OriginalFile.FileName),
-                MimeType = "application/vnd.google-apps.document"
+                MimeType = fileConfiguration.OriginalMimeType
             };
 
             FilesResource.CreateMediaUpload request;
             using (var stream = command.OriginalFile.OpenReadStream())
             {
-                request = service.Files.Create(fileMetadata, stream, "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+                request = service.Files.Create(fileMetadata, stream, fileConfiguration.ContentType);
                 request.Fields = "id";
                 request.Upload();
             }
@@ -43,7 +52,7 @@ namespace Google.BLL.Convert.Commands
 
             if (convertedFileId != null)
             {
-                var exportRequest = service.Files.Export(convertedFileId, "application/pdf");
+                var exportRequest = service.Files.Export(convertedFileId, fileConfiguration.OutputMimeType);
                 var streamResponse = exportRequest.ExecuteAsStream();
 
                 var outputFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
---
+++ Utils.Constants/Configuration/GoogleConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Utils.Constants.Configuration
+{
+    public class GoogleConfiguration
+    {
+        public string ServiceAccountKeyPath { get; set; }
+        public string ApplicationName { get; set; }
+    }
+}
