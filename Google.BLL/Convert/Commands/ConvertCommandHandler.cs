8fe2194cf4a154a3f306df3b53a539c2f7ee99cb
Polly <apollinaria.9203@yandex.by>
2023-07-05 04:28:28 +0300

    Добавьте файлы проекта.

---
+++ Google.BLL/Convert/Commands/ConvertCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using Google.BLL.Convert.Models;
+using MediatR;
+
+namespace Google.BLL.Convert.Commands
+{
+    public class ConvertCommandHandler : IRequestHandler<ConvertCommand, ConvertResult>
+    {
+        public async Task<ConvertResult> Handle (ConvertCommand command, CancellationToken cancellationToken)
+        {
+            return new ConvertResult();
+        }
+    }
+}
---
+++ Google.BLL/DependenciesBootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Google.BLL
+{
+    public static class DependenciesBootstrapper
+    {
+        public static IServiceCollection AddGoogleBll(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            return services;
+        }
+    }
+}
